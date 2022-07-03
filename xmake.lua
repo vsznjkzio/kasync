d448e2ab78ade2d9e0de31eb8a1b686798b5db3e
kapai <luanrengna99523@163.com>
2022-07-03 16:41:48 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -5,23 +5,18 @@
 add_requires("openssl","gtest")
 
 target("kasync")
-if is_plat("windows") then
-    set_kind("static")
-    add_files("./src/*.c")
-    add_includedirs("./include/")
-    add_packages("openssl")
+ set_kind("static")
+ add_files("./src/*.c")
+ add_includedirs("./include/")
+ add_packages("openssl")
+ add_syslinks("ws2_32","advapi32")
+ set_configvar("KSOCKET_SSL",1) --开启ssl
+ set_configvar("KSOCKET_SSL_BIO",1) --开启ssl的bio
+ --set_configvar("WOLFSSL_SSL",1) --开启wolfssl的bio
+ --set_configvar("OPENSSL_EXTRA",1) --开启wolfssl的OPENSSL 兼容
+ set_configdir("./include/")
+ add_configfiles("./kasync_config.h.in")
 
-    add_syslinks("ws2_32","advapi32")
-    set_configvar("KSOCKET_SSL",1) --开启ssl
-    set_configvar("KSOCKET_SSL_BIO",1) --开启ssl的bio
-    --set_configvar("WOLFSSL_SSL",1) --开启wolfssl的bio
-    --set_configvar("OPENSSL_EXTRA",1) --开启wolfssl的OPENSSL 兼容
-
-
-
-    set_configdir("./include/")
-    add_configfiles("./kasync_config.h.in")
-end
 
 target("example")
  set_kind("binary")
@@ -32,7 +27,7 @@ target("example")
  add_files("./example/pingpang.c")
 
 
- target("test")
+target("test")
  set_kind("binary")
  add_packages("openssl","gtest")
  add_syslinks("ws2_32","advapi32")
